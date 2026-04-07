8da9617f98653f69b3a9245b4292efb50f1dfd1f
olium <jeremie.rouxel@outlook.fr>
2026-04-07 19:21:42 +0200

    correction de l'erreur de compilation de la tache 14 & 16

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 
 /**
+ * local_height - Measures the height of a subtree in edges
+ * @tree: Pointer to the subtree root
+ * Return: Height in edges, 0 if tree is NULL or leaf
+ */
+
+static size_t local_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	left_height = (tree->left) ? local_height(tree->left) + 1 : 0;
+	right_height = (tree->right) ? local_height(tree->right) + 1 : 0;
+
+	return (left_height > right_height ? left_height : right_height);
+}
+
+/**
  * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: Pointer to the root node of the tree to measure the balance factor
  * Return: Balance factor of the tree, or 0 if tree is NULL
@@ -14,8 +35,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = (tree->left) ? local_height(tree->left) + 1 : 0;
+	right_height = (tree->right) ? local_height(tree->right) + 1 : 0;
 
-	return (left_height - right_height);
+	return ((int)left_height - (int)right_height);
 }
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,6 +2,41 @@
 #include <stdlib.h>
 
 /**
+ * local_height - Measures the height of a subtree in edges
+ * @tree: Pointer to the subtree root
+ * Return: Height in edges, 0 if tree is NULL or leaf
+ */
+
+static size_t local_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	left_height = (tree->left) ? local_height(tree->left) + 1 : 0;
+	right_height = (tree->right) ? local_height(tree->right) + 1 : 0;
+
+	return (left_height > right_height ? left_height : right_height);
+}
+
+/**
+ * local_size - Counts nodes in a binary subtree
+ * @tree: Pointer to the subtree root
+ * Return: Number of nodes
+ */
+
+static size_t local_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + local_size(tree->left) + local_size(tree->right));
+}
+
+/**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node of the tree to check
  * Return: 1 if the tree is perfect, otherwise 0
@@ -9,15 +44,16 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height, size;
+	size_t height, size, expected_size;
 
 	if (tree == NULL)
 		return (0);
 
-	height = binary_tree_height(tree);
-	size = binary_tree_size(tree);
+	height = local_height(tree);
+	size = local_size(tree);
+	expected_size = ((size_t)1 << (height + 1)) - 1;
 
-	if (size == (1 << height) - 1)
+	if (size == expected_size)
 		return (1);
 	else
 		return (0);
