a797292b7205aca63d537da5d60178755da77751
Rmxpvl <remy.pinville@gmail.com>
2026-04-07 13:48:47 +0200

    AddBinary

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,7 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -8,11 +12,13 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+#endif
